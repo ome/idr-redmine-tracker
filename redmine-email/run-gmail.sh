41dd5fea16884b42c6343a7d82fd1b22e5bd2adc
Simon Li <spli@dundee.ac.uk>
2018-01-16 13:23:10 +0000

    Set default group to submitters for auto-created users

--- redmine-email/run-gmail.sh
+++ redmine-email/run-gmail.sh
@@ -8,6 +8,7 @@ GMAIL_PASS=$(sed -nre 's/^\s*password:\s*"(.+)"/\1/p' /usr/src/redmine/config/co
 GMAIL_FOLDER_SUCCESS=RedmineSuccess
 GMAIL_FOLDER_FAILURE=RedmineFailure
 REDMINE_PROJECT=idr-submission
+REDMINE_GROUPS=submitters
 
 # These environment variables are set in Docker image library/redmine:3.4.4
 # but are lost when cron is run
@@ -18,6 +19,8 @@ export \
     BUNDLE_APP_CONFIG=/usr/local/bundle \
     PATH=PATH=/usr/local/bundle/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
+# See https://github.com/redmine/redmine/blob/3.4.4/lib/tasks/email.rake
+# for explanation of settings
 /usr/local/bundle/bin/rake -f /usr/src/redmine/Rakefile \
     redmine:email:receive_imap \
     RAILS_ENV="production" \
@@ -26,12 +29,13 @@ export \
     password="$GMAIL_PASS" \
     ssl=1 \
     port=993 \
-    unknown_user=accept \
+    unknown_user=create \
     move_on_success="$GMAIL_FOLDER_SUCCESS" \
     move_on_failure="$GMAIL_FOLDER_FAILURE" \
     project="$REDMINE_PROJECT" \
     tracker=Support \
     no_permission_check=1 \
+    default_group="$REDMINE_GROUPS"
 
 if [ $? -ne 0 ]; then
     echo "FAILED"
