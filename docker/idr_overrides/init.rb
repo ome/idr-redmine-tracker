16495a8a27126623be6d77abe74ac5671514a10e
Simon Li <spli@dundee.ac.uk>
2018-07-18 19:26:44 +0100

    Add custom Docker image

---
+++ docker/idr_overrides/init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :idr_overrides do
+  name 'IDR custom overrides'
+  author 'Openmicroscopy'
+  description 'IDR customisations for Redmine'
+  version '0.0.1'
+  url 'https://github.com/openmicroscopy/idr-redmine-tracker'
+  author_url 'https://www.openmicroscopy.org/'
+end
