66829d8e2f70b9b6aa8fe11c3296cc7303f5e73c
Simon Li <spli@dundee.ac.uk>
2018-07-19 11:53:13 +0100

    Figure out how to minimise idr_overrides changes
    Just override the account information text for en and en-GB only.

--- docker/idr_overrides/init.rb
+++ docker/idr_overrides/init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :idr_overrides do
   name 'IDR custom overrides'
   author 'Openmicroscopy'
   description 'IDR customisations for Redmine'
-  version '0.0.1'
+  version '0.1.0'
   url 'https://github.com/openmicroscopy/idr-redmine-tracker'
   author_url 'https://www.openmicroscopy.org/'
 end
